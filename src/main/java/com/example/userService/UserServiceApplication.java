9da734e263a6ff2fbb5926546d80901cb785eedd
ukurilina <ulanakur1@mail.ru>
2025-11-04 21:49:26 +0300

    Liquibase DB schema setup with env-based config

--- src/main/java/com/example/userService/UserServiceApplication.java
+++ src/main/java/com/example/userService/UserServiceApplication.java
@@ -1,13 +1,10 @@
 package com.example.userService;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
 public class UserServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
-
 }
