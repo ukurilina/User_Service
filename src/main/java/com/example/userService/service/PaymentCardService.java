b446ad808dc569c5d0946a54da2086ae9c64cfbb
ukurilina <ulanakur1@mail.ru>
2025-11-12 18:15:35 +0300

    Caching add

--- src/main/java/com/example/userService/service/PaymentCardService.java
+++ src/main/java/com/example/userService/service/PaymentCardService.java
@@ -7,22 +7,27 @@ import com.example.userService.repository.PaymentCardRepository;
 import com.example.userService.repository.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.Optional;
 import java.util.List;
 
 @Service
-@Transactional
 public class PaymentCardService {
+
     private final PaymentCardRepository paymentCardRepository;
     private final UserRepository userRepository;
 
-    public PaymentCardService(PaymentCardRepository paymentCardRepository,
-                              UserRepository userRepository) {
+    public PaymentCardService(PaymentCardRepository paymentCardRepository, UserRepository userRepository) {
         this.paymentCardRepository = paymentCardRepository;
         this.userRepository = userRepository;
     }
+
+    @CacheEvict(value = {"userCardsCache"}, key = "#userId")
     public PaymentCard createCard(PaymentCard card, Long userId) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User is not found"));
@@ -36,18 +41,26 @@ public class PaymentCardService {
         return paymentCardRepository.save(card);
     }
 
+    @Cacheable(value = "cards", key = "#id")
+    @Transactional(readOnly = true)
     public Optional<PaymentCard> getCardById(Long id) {
         return paymentCardRepository.findById(id);
     }
 
+    @Cacheable(value = "cards")
+    @Transactional(readOnly = true)
     public Page<PaymentCard> getAllCards(Pageable pageable) {
         return paymentCardRepository.findAllCards(pageable);
     }
 
+    @Cacheable(value = "userCardsCache", key = "#userId")
+    @Transactional(readOnly = true)
     public List<PaymentCard> getCardsByUserId(Long userId) {
         return paymentCardRepository.findByUserId(userId);
     }
 
+    @CachePut(value = "cards", key = "#id")
+    @Transactional
     public PaymentCard updateCard(Long id, PaymentCard cardDetails) {
         return paymentCardRepository.findById(id)
                 .map(card -> {
@@ -56,13 +69,22 @@ public class PaymentCardService {
                     card.setExpirationDate(cardDetails.getExpirationDate());
                     return paymentCardRepository.save(card);
                 })
-                .orElseThrow(() -> new RuntimeException("Card is not found"));
+                .orElseThrow(() -> new PaymentCardNotFoundException(id));
     }
+
+    @CachePut(value = "cards", key = "#id")
+    @Transactional
     public void activateOrDeactivateCard(Long id, Boolean active) {
         PaymentCard card = paymentCardRepository.findById(id)
                 .orElseThrow(() -> new PaymentCardNotFoundException(id));
         paymentCardRepository.updateActiveStatus(id, active);
     }
+
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "cards", key = "#id"),
+            @CacheEvict(value = "userCardsCache", key = "#userId")
+    })
     public void deleteCard(Long id) {
         PaymentCard card = paymentCardRepository.findById(id)
                 .orElseThrow(() -> new PaymentCardNotFoundException(id));
