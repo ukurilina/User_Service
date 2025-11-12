a1bc2617553b541d8ca276eb33d65cd55b420420
ukurilina <ulanakur1@mail.ru>
2025-11-12 19:55:04 +0300

    DTO controllers

--- src/main/java/com/example/userService/controller/UserController.java
+++ src/main/java/com/example/userService/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.userService.controller;
 
 import com.example.userService.dto.UserDTO;
-import com.example.userService.mapper.UserMapper;
 import com.example.userService.service.UserService;
 import com.example.userService.exception.UserNotFoundException;
 import org.springframework.data.domain.Page;
@@ -14,45 +13,36 @@ import org.springframework.http.ResponseEntity;
 @RequestMapping("/api/users")
 public class UserController {
     private final UserService userService;
-    private final UserMapper userMapper;
 
-    public UserController(UserService userService, UserMapper userMapper) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userMapper = userMapper;
     }
-
 
     @PostMapping
     public UserDTO createUser(@RequestBody UserDTO userDTO) {
-        var user = userMapper.toEntity(userDTO);
-        var createdUser = userService.createUser(user);
-        return userMapper.toDTO(createdUser);
+        return userService.createUser(userDTO);
     }
 
     @GetMapping("/{id}")
     public UserDTO getUserById(@PathVariable Long id) {
-        var user = userService.getUserById(id)
+        return userService.getUserById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
-        return userMapper.toDTO(user);
     }
 
     @GetMapping
     public Page<UserDTO> getAllUsers(
             @RequestParam(required = false) String firstName,
             @RequestParam(required = false) String surname,
-            @RequestParam int page,
-            @RequestParam int size) {
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
         Pageable pageable = PageRequest.of(page, size);
-        var usersPage = userService.getAllUsers(firstName, surname, pageable);
-        return usersPage.map(userMapper::toDTO);
+        return userService.getAllUsers(firstName, surname, pageable);
     }
 
     @PutMapping("/{id}")
     public UserDTO updateUser(@PathVariable Long id, @RequestBody UserDTO userDTO) {
-        var user = userMapper.toEntity(userDTO);
-        var updatedUser = userService.updateUser(id, user);
-        return userMapper.toDTO(updatedUser);
+        return userService.updateUser(id, userDTO);
     }
 
     @PatchMapping("/{id}/active")
