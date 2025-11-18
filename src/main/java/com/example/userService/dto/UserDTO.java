00393ffd83ffab502c44cf8a2491e4351c66c969
ukurilina <ulanakur1@mail.ru>
2025-11-18 16:51:04 +0300

    lombok added got userDto

--- src/main/java/com/example/userService/dto/UserDTO.java
+++ src/main/java/com/example/userService/dto/UserDTO.java
@@ -28,28 +28,6 @@ public class UserDTO {
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-    private List<PaymentCardDTO> paymentCards = new ArrayList<>();
-
-    public Long getId() {return id; }
-    public void setId(Long id) {this.id = id; }
-
-    public String getName() {return name; }
-    public void setName(String name) {this.name = name; }
-
-    public String getSurname() {return surname; }
-    public void setSurname(String surname) {this.surname = surname; }
-
-    public LocalDate getBirthDate() {return birthDate; }
-    public void setBirthDate(LocalDate birthdate) {this.birthDate = birthdate; }
-
-    public String getEmail() {return email; }
-    public void setEmail(String email) {this.email = email; }
-
-    public Boolean getActive() {return active; }
-    public void setActive(Boolean active) {this.active = active; }
-
-    public LocalDateTime getCreatedAt() {return createdAt;}
-    public void setCreatedAt(LocalDateTime createdAt) {this.createdAt = createdAt; }
 
     public LocalDateTime getUpdatedAt() {return updatedAt;}
     public void setUpdatedAt(LocalDateTime updatedAt) {this.updatedAt = updatedAt; }
